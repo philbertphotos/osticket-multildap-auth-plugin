946edcb5893b18c87e78dd561f68f6f21f526af0
Joseph Philbert <joe@philbertphotos.com>
2020-10-15 22:53:09 -0400

    Update plugin.php
    minor changes.

--- multi-ldap/plugin.php
+++ multi-ldap/plugin.php
@@ -1,8 +1,8 @@
 <?php
 return array(
     'id' =>             'multiauth:ldap',
-    'version' =>        '1.3',
-    'name' =>           'Multi LDAP Authentication and Lookup',
+    'version' =>        '1.3.1',
+    'name' =>           'Multi-LDAP Authentication',
     'author' =>         'Joseph Philbert',
     'description' =>    'Provides a configurable authentication backend which works against multiple LDAP servers',
     'url' =>            'http://www.vide.vi',
