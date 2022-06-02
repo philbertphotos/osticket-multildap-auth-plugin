156326c33bbe25cd1a119784c7a3d606b6a3b727
Joseph Philbert <joe@philbertphotos.com>
2022-06-01 21:38:26 -0400

    Update plugin.php

--- multi-ldap/plugin.php
+++ multi-ldap/plugin.php
@@ -1,7 +1,7 @@
 <?php
 return array(
     'id' =>             'multiauth:ldap',
-    'version' =>        '1.5.1',
+    'version' =>        '1.5.8',
     'name' =>           'Multi LDAP Authentication and Lookup',
     'author' =>         'Joseph Philbert',
     'description' =>    'Provides a configurable authentication backend which works against multiple LDAP servers',
