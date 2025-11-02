56b6c5c71ee13d657433e99f91f7cd16cc8716d4
Joseph Philbert <joe@philbertphotos.com>
2025-11-02 04:00:09 -0400

    Update plugin version from 1.9.15 to 1.10

--- multi-ldap/plugin.php
+++ multi-ldap/plugin.php
@@ -1,12 +1,11 @@
-<?php
 return array(
     'id' =>             'multiauth:ldap',
-    'version' =>        '1.9.15',
+    'version' =>        '1.10',
 	'ost_version' =>    '1.16', # Require osTicket v1.17+
     'name' =>           'Multi LDAP Authentication and Lookup',
     'author' =>         'Joseph Philbert',
     'description' =>    'Provides a configurable authentication backend which works against multiple LDAP servers',
     'url' =>            'http://www.vide.vi',
-    'plugin' =>         'auth.php:LdapMultiAuthPlugin'
+    'plugin' =>         'auth.php:LdapMultiAuthPlugin',
+    //'requires' => array("sync/sync_mldap.php" => array("version" => "*","map" => array("sync/sync_mldap.php/src" => 'sync/',),),)	
 );
-?>
